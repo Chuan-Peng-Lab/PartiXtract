7278713bc7aa5e9eb4cd9bb1b6cfdc5ac6eab598
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-04-28 17:02:37 +0300

    Update PaperReader.py

--- PaperReader.py
+++ PaperReader.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 # api = 'sk-#################'
-api = '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'
+api = ''
 
 
 class PaperReader:
@@ -23,6 +23,7 @@ class PaperReader:
             self.paper_abstract = ''
             self.paper_content = ''
             self.paper_references = ''
+            self.paper_participants = ''
 
         def all_text(self):
             return self.paper_name+'\n\n' + self.paper_authors + '\n\n' + \
@@ -44,15 +45,6 @@ class PaperReader:
         else:
             raise Exception("File type not supported")
 
-    """ 未完成
-    def readPDF(self):
-        pdf = fitz.open(self.path)  # pdf文档
-        text_list = [page.get_text() for page in pdf]
-        text = ' '.join(text_list)
-        pdf.close()
-        return text 
-    """
-
     def readURL(self):
         driver_op = Options()
         driver_op.add_argument('--window-size=1920,1080')
@@ -65,61 +57,53 @@ class PaperReader:
         driver.get(self.path)
         time.sleep(2)
         # Accept All
-        driver.find_element(
-            By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/button[3]").click()
+        accept = driver.find_elements(
+            By.XPATH, "/html/body/div")
+        
 
-        # Show all of names
-        driver.find_element(
-            By.XPATH, "//main/article/header/div/div[2]/span/span/span[3]/span").click()
-
+        for i in accept:
+            if "ACCEPT ALL" in i.text:
+                i.find_element(By.XPATH,"//div/div/div/div[2]/div/button[3]").click()
+            
         paper = self.Paper()
 
-        # Get data from website
-        paper.paper_name = driver.find_element(
-            By.XPATH, "//main/article/header/div/h1").text
-        paper.paper_authors = driver.find_element(
-            By.XPATH, "//main/article/header/div/div[2]/span").text.replace('\n', '').replace('\r', '')
-        paper.paper_abstract = driver.find_element(
-            By.XPATH, "//main/article/div[2]/div").text
-        paper.paper_content = driver.find_element(
-            By.XPATH, "//main/article/section[1]").text
         paper.paper_references = driver.find_element(
             By.XPATH, "//main/article/section[2]").text
-
+        temp = driver.find_elements(By.TAG_NAME, 'section')
+        for i in temp:
+            if i.text.startswith("Participants"):
+                paper.paper_participants = i.text
+                break
         paper.paper_url = self.path
         return paper
 
     def GPT_Paper(self):
         openai.api_key = api
         messages = [
-            {"role": "system", "content": "You are a researcher who is good at summarizing psychology papers in concise sentences"},
-            {"role": "assistant", "content": "This is the title: " + self.paper.paper_name + ", author: " + self.paper.paper_authors + ", link: " + self.paper.paper_url +
-                ", abstract: " + self.paper.paper_abstract + " of an English document. I need your help to read and summarize the following questions: "},
-            {"role": "user", "content": """                 
-                 1. Mark the title of the paper (with Chinese translation)
-                 2. list all the authors' names (use English)
-                 3. mark the first author's affiliation (output {} translation only)                 
-                 4. mark the keywords of this article (use English)
-                 5. link to the paper, Github code link (if available, fill in Github:None if not)
-                 6. summarize according to the following four points.Be sure to use {} answers (proper nouns need to be marked in English)
-                    - (1):What is the research background of this article?
-                    - (2):What are the past methods? What are the problems with them? Is the approach well motivated?
-                    - (3):What is the research methodology proposed in this paper?
-                    - (4):On what task and what performance is achieved by the methods in this paper? Can the performance support their goals?
-                 Follow the format of the output that follows:                  
-                 1. Title: xxx\n\n
-                 2. Authors: xxx\n\n
-                 3. Affiliation: xxx\n\n                 
-                 4. Keywords: xxx\n\n   
-                 5. Urls: xxx or xxx , xxx \n\n      
-                 6. Summary: \n\n
-                    - (1):xxx;\n 
-                    - (2):xxx;\n 
-                    - (3):xxx;\n  
-                    - (4):xxx.\n\n     
-                 
-                 Be sure to use {} answers (proper nouns need to be marked in English), statements as concise and academic as possible, do not have too much repetitive information, numerical values using the original numbers, be sure to strictly follow the format, the corresponding content output to xxx, in accordance with \n line feed.                 
-                 """.format(self.language, self.language, self.language)},
+            {"role": "system", "content": "You are a researcher who is good at summarizing information about subjects in psychology papers in concise sentences"},
+            {"role": "assistant", "content": "This is the paragraph in the text that contains the subject's information: <" +
+             self.paper.paper_participants
+             + ">, please extract information from it, including but not limited to age, gender, region, race and education level, please try to answer the following questions"},
+
+            {"role": "user", "content": """ 
+         Please refer to the format of the following example to output the subject information of the paper based on the information I provided:
+         "Fourteen self-identified Chinese Buddhists (seven males, seven females, 21–31 years of age, mean 25.4, sd 2.46) participated in this study as paid volunteers. The participants had been attached to local faith communities for 1–7 years (mean 2.5, sd 2.0) when they participated in this study. These participants are recruited from Shanxi Province, China. Eleven participants reported to attend the community activity at least once a week. Twelve participants reported to cultivate themselves according to Mahayana (one of the major schools of Buddhism) doctrine everyday. Ten participants reported to read sutra everyday. The participants were asked to rate the importance they placed on religion and their attitude toward Buddha, based on a 5-point scale (0 ¼ not important or do not believe at all, 4 ¼ very important or strongly believe), resulting a mean rating score of 3.56. All participants had no neurological or psychiatric history. All participants had college education, were right-handed and had normal or corrected-to-normal vision. Informed consent was obtained from all participants prior to scanning. This study was approved by a local ethics committee.",
+                [
+                    {
+                        "sample_size": 14,
+                        "female": 7,
+                        "mean_age": 25.4,
+                        "age_span": "23-31 and 25.4±2.46",
+                        "education": "college education",
+                        "race": "Chinese",
+                        "area": "Shanxi Province, China",
+                        "socioeconomic": "NA",
+                        "remuneration": "NA",
+                        "handedness": "right-handed"
+                    },
+                ],                
+                
+                """},
         ]
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -128,7 +112,7 @@ class PaperReader:
         result = ''
         for choice in response.choices:
             result += choice.message.content
-        print("method_result:\n", result, end="\n\n")
+        print(result, end="\n\n")
         print("prompt_token_used:", response.usage.prompt_tokens,
               "completion_token_used:", response.usage.completion_tokens,
               "total_token_used:", response.usage.total_tokens, end="\n")
@@ -138,8 +122,8 @@ class PaperReader:
 
 if __name__ == "__main__":
     PR = PaperReader(
-        "https://journals.sagepub.com/doi/full/10.1177/1948550620902281", language="en-us")
+        "https://journals.sagepub.com/doi/full/10.1177/19485506221107268", language="en-us")
     GPT_result = PR.GPT_Paper()
-    with open("./test", "w") as file:
+    with open("./test.txt", "w") as file:
         file.write(GPT_result)
     # PR.paper.saveText("paper.txt")
